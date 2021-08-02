c993f6764051e8ab3daab44e935f96d904bdbf48
Ran Cohen <cohenran@gmail.com>
2021-08-02 07:07:49 +0300

    Better working version

--- src/main/java/com/mabaya/sponserads/dao/CampaignRepository.java
+++ src/main/java/com/mabaya/sponserads/dao/CampaignRepository.java
@@ -4,6 +4,9 @@ import com.mabaya.sponserads.model.CampaignEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CampaignRepository extends CrudRepository<CampaignEntity, Integer> {
+	List<CampaignEntity> findByName(String name);
 }
--- src/main/java/com/mabaya/sponserads/model/CampaignEntity.java
+++ src/main/java/com/mabaya/sponserads/model/CampaignEntity.java
@@ -4,12 +4,15 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import java.time.LocalDate;
 import java.util.List;
 
 @Data
+@Entity
 @NoArgsConstructor
 @AllArgsConstructor
 public class CampaignEntity {
@@ -18,6 +21,7 @@ public class CampaignEntity {
 	private Integer id;
 	
 	private String name;
+	@OneToMany
 	private List<ProductEntity> productEntities;
 	private LocalDate startDate;
 	private Float bid;
